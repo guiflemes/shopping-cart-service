f4cde3710024e80f0f90da9909ed150eb454bfce
Guilherme Silva <guilherme.1995lemes@gmail.com>
2022-09-24 21:05:50 -0300

    create AddItemCommand

--- src/app/command/addItem.go
+++ src/app/command/addItem.go
@@ -1,3 +1,27 @@
 package command
 
-type AddItem struct{}
+import (
+	"context"
+	"errors"
+	"shopping_cart/src/common/decorator"
+
+	"github.com/sirupsen/logrus"
+)
+
+type AddItemCommand struct{}
+
+type AddItemHandler decorator.CommandHandler[AddItemCommand]
+
+type addItemHandler struct{}
+
+func NewAddItemHandler(logger *logrus.Entry, metricsClient decorator.MetricsClient) AddItemHandler {
+	return decorator.ApplyCommandDecorator[AddItemCommand](
+		addItemHandler{},
+		logger,
+		metricsClient,
+	)
+}
+
+func (c addItemHandler) Handle(ctx context.Context, cmd AddItemCommand) error {
+	return errors.New("any error")
+}
